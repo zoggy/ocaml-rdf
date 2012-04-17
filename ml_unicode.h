5f10c3fe87f5240116dce35e935e2462fe4e5347
zoggy <zoggy@bat8.org>
2012-04-17 11:32:04 +0200

    remove files; will do a native ocaml library

--- ml_unicode.h
+++
@@ -1,7 +0,0 @@
-/* */
-#include "ml_types.h"
-
-#ifndef ML_UNICODE
-#define ML_UNICODE
-
-#endif
--- tests/unicode.ml
+++
@@ -1,8 +0,0 @@
-
-let s = "Il était une fois ... près de la fontaine ... un mouton tout pâle";;
-
-let utf8 = Rdf_unicode.latin1_to_utf8 s;;
-let latin1 = Rdf_unicode.utf8_to_latin1 utf8;;
-
-prerr_endline
-(Printf.sprintf "original=%s\nutf8=%s\nlatin1=%s" s utf8 latin1);;
