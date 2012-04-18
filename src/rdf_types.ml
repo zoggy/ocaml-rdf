ccfc34a41a1a1004d1f2cca1d8bb71d139249951
zoggy <zoggy@bat8.org>
2012-04-18 11:54:18 +0200

    export uri

--- src/rdf_types.ml
+++ src/rdf_types.ml
@@ -1,6 +1,6 @@
 (** *)
 
-type uri = string
+
 type literal = {
     lit_value : string ;
     lit_language : string option ;
@@ -9,19 +9,17 @@ type literal = {
 type blank_id = string
 
 type node =
-  | Uri of uri
+  | Uri of Rdf_uri.uri
   | Literal of literal
   | Blank
   | Blank_ of blank_id
 
 type triple = node * node * node
 
-let string_of_uri uri = uri;;
-let uri_of_string str = str;;
 let string_of_blank_id id = id;;
 let blank_id_of_string str = str;;
 
-let node_of_uri_string s = Uri (uri_of_string s);;
+let node_of_uri_string s = Uri (Rdf_uri.uri s);;
 let mk_literal ?typ ?lang v =
   { lit_value = v ; lit_language = lang ; lit_type = typ ; }
 ;;
@@ -30,7 +28,7 @@ let node_of_literal_string ?typ ?lang v =
 ;;
 
 let string_of_node = function
-| Uri uri -> Printf.sprintf "<%s>" uri
+| Uri uri -> Printf.sprintf "<%s>" (Rdf_uri.string uri)
 | Literal lit ->
     Printf.sprintf "%S%s%s"
     lit.lit_value
@@ -39,19 +37,11 @@ let string_of_node = function
      | Some l -> Printf.sprintf "@%s" l)
     (match lit.lit_type with
        None -> ""
-     | Some t -> Printf.sprintf "^^%s" t)
+     | Some t -> Printf.sprintf "^^%s" (Rdf_uri.string t))
 | Blank -> "_"
-| Blank_ id -> Printf.sprintf "_:%s" id
+| Blank_ id -> Printf.sprintf "_:%s" (string_of_blank_id id)
 ;;
 
 
-type options = (string * string) list
-let get_option ?def name l =
-  try List.assoc name l
-  with Not_found ->
-    match def with
-        None -> failwith (Printf.sprintf "Missing option %S" name)
-      | Some v -> v
-;;
 
 
--- src/rdf_types.mli
+++ src/rdf_types.mli
@@ -1,34 +1,27 @@
 (** Base types. *)
 
-type uri
 type literal = {
   lit_value : string;
   lit_language : string option;
-  lit_type : uri option;
+  lit_type : Rdf_uri.uri option;
 }
 
 type blank_id
 
 type node =
-  | Uri of uri
+  | Uri of Rdf_uri.uri
   | Literal of literal
   | Blank
   | Blank_ of blank_id
 
 type triple = node * node * node
 
-val string_of_uri : uri -> string
-val uri_of_string : string -> uri
-
 val string_of_blank_id : blank_id -> string
 val blank_id_of_string : string -> blank_id
 
 val node_of_uri_string : string -> node
 
-val mk_literal : ?typ:uri -> ?lang:string -> string -> literal
-val node_of_literal_string : ?typ:uri -> ?lang:string -> string -> node
+val mk_literal : ?typ:Rdf_uri.uri -> ?lang:string -> string -> literal
+val node_of_literal_string : ?typ:Rdf_uri.uri -> ?lang:string -> string -> node
 
 val string_of_node : node -> string
-
-type options = (string * string) list
-val get_option : ?def:string-> string -> options -> string
