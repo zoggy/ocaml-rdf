f1a6a7b0d27350e7bbff0b52cc7172db5ec1e0ed
zoggy <zoggy@bat8.org>
2012-09-25 15:52:25 +0200

    continue rdf/xml parser

--- src/rdf_xml.ml
+++ src/rdf_xml.ml
@@ -22,6 +22,25 @@ let out_tree o t =
   in
   Xmlm.output_doc_tree frag o t
 
+let string_of_xmls trees =
+  try
+    let b = Buffer.create 256 in
+    let output = Xmlm.make_output ~decl: false (`Buffer b) in
+    let frag = function
+    | E (tag, childs) -> `El (tag, childs)
+    | D d -> `Data d
+    in
+    List.iter (fun tree -> Xmlm.output_doc_tree frag output (None, tree)) trees;
+    Buffer.contents b
+  with
+    Xmlm.Error ((line, col), error) ->
+      let msg = Printf.sprintf "Line %d, column %d: %s"
+        line col (Xmlm.error_message error)
+      in
+      failwith msg
+;;
+
+
 (** {2 Constants defined in
   {{:http://www.w3.org/TR/rdf-syntax-grammar/}RDF grammar}. } *)
 
@@ -30,7 +49,7 @@ let rdf_ s = Rdf_uri.uri ("http://www.w3.org/1999/02/22-rdf-syntax-ns#"^s);;
 (** {3 Syntax names} *)
 let rdf_about = rdf_"about";;
 let rdf_datatype = rdf_"datatype";;
-let rdf_Descriptin = rdf_"Description";;
+let rdf_Description = rdf_"Description";;
 let rdf_ID = rdf_"ID";;
 let rdf_li = rdf_"li";;
 let rdf_nodeID = rdf_"nodeID";;
@@ -69,7 +88,7 @@ let rdf_nil = rdf_"nil"
 (** {2 Input} *)
 
 type state =
-  { subject : Rdf_uri.uri option ;
+  { subject : Rdf_node.node option ;
     predicate : Rdf_uri.uri option ;
     xml_base : Rdf_uri.uri ;
     xml_lang : string option ;
@@ -115,7 +134,7 @@ let get_blank_node g gstate id =
   try (Blank_ (SMap.find id gstate.blanks), gstate)
   with Not_found ->
     let bid = g.new_blank_id () in
-    let gstate = { gstate with blanks = SMap.add id bid gstate.blanks } in
+    let gstate = { (*gstate with*) blanks = SMap.add id bid gstate.blanks } in
     (Blank_ bid, gstate)
 
 
@@ -123,12 +142,12 @@ let rec input_node g state gstate t =
   let state = update_state state t in
   match t with
     D s when state.predicate = None ->
-      let msg = Printf.sprintf "Found Data (%S) with no current predicate." s in
+      let msg = Printf.sprintf "Found (Data %S) with no current predicate." s in
       error msg
   | D s ->
       let obj = Rdf_node.node_of_literal_string ?typ: state.datatype ?lang: state.xml_lang s in
-      let sub = match state.subject with None -> assert false | Some s -> Uri s in
-      let pred = match state.subject with None -> assert false | Some u -> Uri u in
+      let sub = match state.subject with None -> assert false | Some s -> s in
+      let pred = match state.predicate with None -> assert false | Some u -> Uri u in
       g.add_triple ~sub ~pred ~obj;
       gstate
   | E (((pref,s), atts), children) ->
@@ -141,11 +160,90 @@ let rec input_node g state gstate t =
             | None ->
                 match get_att_uri rdf_nodeID atts with
                   Some id -> get_blank_node g gstate id
-                | None -> (Blank, gstate)
+                | None -> (Blank_ (g.new_blank_id ()), gstate)
       in
-      assert false
+      begin
+        match state.subject, state.predicate with
+          Some sub, Some pred ->
+            g.add_triple ~sub ~pred: (Uri pred) ~obj: node
+        | _ -> ()
+      end;
+      let state = { state with subject = Some node ; predicate = None } in
+      (* add a type arc if the node is not introduced with rdf:Description *)
+      if not (is_element rdf_Description (pref,s)) then
+        begin
+          let type_uri = Rdf_uri.concat (Rdf_uri.uri pref) s in
+          g.add_triple ~sub: node ~pred: (Uri rdf_type) ~obj: (Uri type_uri)
+        end;
+      (* all remaining attributes define triples with literal object values *)
+      let f ((pref, s), v) =
+        if pref <> Xmlm.ns_xml && pref <> Xmlm.ns_xmlns then
+          begin
+            let uri_prop = Rdf_uri.concat (Rdf_uri.uri pref) s in
+            if not (List.exists (Rdf_uri.equal uri_prop) [ rdf_about ; rdf_ID ; rdf_nodeID ]) then
+              begin
+                let obj = Rdf_node.node_of_literal_string ?lang: state.xml_lang v in
+                g.add_triple ~sub: node ~pred: (Uri uri_prop) ~obj
+              end
+          end
+      in
+      List.iter f atts;
+      List.fold_left (input_prop g state) gstate children
 
-and input_prop g state gstate t = gstate
+(* FIXME: handle rdf:nodeID *)
+(* FIXME: handle rdf:li *)
+and input_prop g state gstate t =
+  let state = update_state state t in
+  match t with
+    D s ->
+      let msg = Printf.sprintf "Found (Data %S) but expected a property node." s in
+      error msg
+  | E (((pref,s),atts),children) ->
+      let sub = match state.subject with None -> assert false | Some sub -> sub in
+      let prop_uri = Rdf_uri.concat (Rdf_uri.uri pref) s in
+      match get_att_uri rdf_resource atts with
+        Some s ->
+          let obj = Uri (Rdf_uri.uri s) in
+          g.add_triple ~sub ~pred: (Uri prop_uri) ~obj ;
+          gstate
+      | None ->
+          match get_att_uri rdf_parseType atts with
+            Some "Literal" ->
+              let xml = string_of_xmls children in
+              let obj = Rdf_node.node_of_literal_string ~typ: rdf_XMLLiteral xml in
+              g.add_triple ~sub ~pred: (Uri prop_uri) ~obj;
+              gstate
+          | Some "Resource" -> failwith "rdf:Resource not handled"
+          | Some s -> error (Printf.sprintf "Unknown parseType %S" s)
+          | None ->
+              match get_att_uri rdf_datatype atts, children with
+                Some s, [D lit] ->
+                  let typ = Rdf_uri.uri s in
+                  let obj = Rdf_node.node_of_literal_string ~typ ?lang: state.xml_lang lit in
+                  g.add_triple ~sub ~pred: (Uri prop_uri) ~obj;
+                  gstate
+              | None, _ ->
+                  (* if we have other attributes than the ones filtered above, they
+                    are property relations, with ommited blank nodes *)
+                  let pred ((pref,s),v) = pref <> Xmlm.ns_xml && pref <> Xmlm.ns_xmlns in
+                  match List.filter pred atts with
+                    [] ->
+                      let state = { state with predicate = Some prop_uri } in
+                      List.fold_left (input_node g state) gstate children
+                  | l ->
+                      let node = Blank_ (g.new_blank_id ()) in
+                      g.add_triple ~sub ~pred: (Uri prop_uri) ~obj: node ;
+                      let f ((pref,s),lit) =
+                        let obj = Rdf_node.node_of_literal_string ?lang: state.xml_lang lit in
+                        let uri_prop = Rdf_uri.concat (Rdf_uri.uri pref) s in
+                        g.add_triple ~sub: node ~pred: (Uri uri_prop) ~obj
+                      in
+                      List.iter f l;
+                      gstate
+
+
+
+;;
 
 let input g ~base t =
   let state = {
