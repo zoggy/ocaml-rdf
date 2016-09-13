f82fe985b13af9c073b1885813c5b5db4de797b8
zoggy <zoggy@bat8.org>
2016-09-13 16:31:53 +0200

    new Rdf_utils module

---
+++ src/rdf_utils.ml
@@ -0,0 +1,41 @@
+(*********************************************************************************)
+(*                OCaml-RDF                                                      *)
+(*                                                                               *)
+(*    Copyright (C) 2012-2016 Institut National de Recherche en Informatique     *)
+(*    et en Automatique. All rights reserved.                                    *)
+(*                                                                               *)
+(*    This program is free software; you can redistribute it and/or modify       *)
+(*    it under the terms of the GNU Lesser General Public License version        *)
+(*    3 as published by the Free Software Foundation.                            *)
+(*                                                                               *)
+(*    This program is distributed in the hope that it will be useful,            *)
+(*    but WITHOUT ANY WARRANTY; without even the implied warranty of             *)
+(*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *)
+(*    GNU General Public License for more details.                               *)
+(*                                                                               *)
+(*    You should have received a copy of the GNU General Public License          *)
+(*    along with this program; if not, write to the Free Software                *)
+(*    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA                   *)
+(*    02111-1307  USA                                                            *)
+(*                                                                               *)
+(*    Contact: Maxence.Guesdon@inria.fr                                          *)
+(*                                                                               *)
+(*********************************************************************************)
+
+(** *)
+
+module G = Rdf_graph
+
+open Rdf_term
+
+let title g sub =
+  match G.literal_objects_of g
+    ~sub:(Iri sub) ~pred: Rdf_dc.title
+  with
+    [] -> None
+  | l ->
+      try
+        let lit = List.find (fun lit -> lit.lit_language = None) l in
+        Some lit.lit_value
+      with Not_found -> Some (List.hd l).lit_value
+      
